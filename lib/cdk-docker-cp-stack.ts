d243b676b9dceaf4c65148a04b6d31834c01cd9b
Kraig Amador <kamador@goodrx.com>
2021-02-24 15:59:20 -0800

    init

--- lib/cdk-docker-cp-stack.ts
+++ lib/cdk-docker-cp-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import { tmpdir } from "os";
 
 export class CdkDockerCpStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const image = cdk.BundlingDockerImage.fromAsset(`${__dirname}/../image`);
+
+    image.cp('/hello.txt', `${tmpdir()}/hello.txt`);
   }
 }
